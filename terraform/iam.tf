bd75b433696efbbe53f0a080e19e90c557757c73
Sarah-Dankwa <sarah_dankwa@hotmail.co.uk>
2025-02-20 20:30:14 +0000

    Added terraform cloudwatch info

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -51,4 +51,29 @@ resource "aws_iam_role_policy_attachment" "sqs_stream_policy_attachment" {
   policy_arn = aws_iam_policy.sqs_policy_stream.arn
 }
 
-########################################## SQS ###########################################################
+# ==========================================
+# CloudWatch Logs Policy for Stream Lambda
+# ==========================================
+
+//The IAM Policy Document specifies the permissions required for stream Lambda to access cloudwatch
+data "aws_iam_policy_document" "cw_document_stream" {
+  statement {
+    effect   = "Allow"
+    actions  = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.stream_log_group.name}:*"]
+  }
+}
+
+// Set up terraform IAMS permissions for Lambda - Cloudwatch
+resource "aws_iam_policy" "cw_policy_stream" {
+  name_prefix = "cw-policy-${var.stream_lambda}"
+  policy      = data.aws_iam_policy_document.cw_document_stream.json
+}
+
+# Attach the CW Policy to the Stream Role
+resource "aws_iam_role_policy_attachment" "cw_policy_attachment_stream" {
+  role       = aws_iam_role.stream_lambda_role.name
+  policy_arn = aws_iam_policy.cw_policy_stream.arn
+}
+
+
--- terraform/var.tf
+++ terraform/var.tf
@@ -2,3 +2,13 @@ variable "stream_lambda" {
   type    = string
   default = "stream"
 }
+
+variable "metric_namespace" {
+  type = string
+  default = "stream_metric"
+}
+
+variable "log_group_name" {
+  type = string
+  default = "stream_log_group"
+}
